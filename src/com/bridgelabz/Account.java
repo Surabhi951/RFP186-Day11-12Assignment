30d7d2f49bc6844200d8be57460d405ee57b3016
Surabhi951 <surabhibhagat95@gmail.com>
2022-08-22 00:03:55 +0530

    Added account to check balance

---
+++ src/com/bridgelabz/Account.java
@@ -0,0 +1,24 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class Account {
+    Scanner sc = new Scanner(System.in);
+    double balance;
+
+    public Account(double balance) {
+        super();
+        this.balance = balance;
+    }
+
+    public void debit(double balance) {
+        System.out.println("Enter amount to withdraw");
+        double withdraw = sc.nextDouble();
+        if (withdraw > balance)
+            System.out.println("Withdraw exceeds the balance please enter correct amount");
+        else
+            balance = balance - withdraw;
+        System.out.println("Remaining balance is " + balance);
+
+    }
+}
